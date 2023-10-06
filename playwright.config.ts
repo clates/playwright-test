0966fda79630f5e51d28ec5d686f6a9e39693204
Christopher Lates <clates@crunchbase.com>
2023-10-06 17:11:12 -0400

    lala

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,11 @@ export default defineConfig({
     baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+
+    // ...
+    trace: "on",
+    video: "on",
+    screenshot: "on",
   },
 
   /* Configure projects for major browsers */
